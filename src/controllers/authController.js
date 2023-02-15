3629a965db38f96d3f0e3e6434a5eb7e60e3578a
BehkoVasilev <vencislavvsilev@gmail.com>
2023-02-15 15:40:40 +0200

    add auth controller
    show login and register page

---
+++ src/controllers/authController.js
@@ -0,0 +1,16 @@
+exports.getRegisterController = (req, res) => {
+    res.render('auth/register');
+
+};
+
+exports.postRegisterController = (req, res) => {
+
+};
+
+exports.getLoginController = (req, res) => {
+    res.render('auth/login')
+};
+
+exports.postLoginController = (req, res) => {
+
+};
--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,19 @@
 const router = require('express').Router();
 const { getHomeController } = require('./controllers/homeController');
+const authControler = require('./controllers/authController')
 
 
 
 
 router.get('/', getHomeController);
-
 router.get('/crypto/search', (req, res) => {
     res.render('search');
 });
+
+router.get('/register', authControler.getRegisterController);
+router.post('/register', authControler.postRegisterController);
+router.get('/login', authControler.getLoginController);
+router.post('/login', authControler.postLoginController);
 
 
 module.exports = router;
