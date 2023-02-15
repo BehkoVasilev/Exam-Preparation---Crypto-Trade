90b03decb888cc55438c6af01b1b51a05620bb66
BehkoVasilev <vencislavvsilev@gmail.com>
2023-02-15 15:29:35 +0200

    add user model

---
+++ src/models/User.js
@@ -0,0 +1,21 @@
+const mognoose = require('mongoose');
+
+const userSchema = new mognoose.Schema({
+    username: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true
+    }
+
+})
+
+const User = mognoose.model('User', userSchema);
+
+module.exports = User;
